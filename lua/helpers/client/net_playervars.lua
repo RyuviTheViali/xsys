6b94352ab411267b05a72d83cd803db2b04af741
sauermon <sauer@sauermaster.com>
2019-09-27 18:21:33 -0500

    Added Net_PlayerVars

---
+++ lua/helpers/client/net_playervars.lua
@@ -0,0 +1,60 @@
+local PLAYER = FindMetaTable("Player")
+local Tag = "NetData"
+local data_table = {}
+
+local net_playervar_debug = CreateConVar("net_playervar_debug","0")
+
+local function Set(id,key,value)
+	local tt = data_table[id]
+
+	if not tt then
+		tt = {}
+		data_table[id] = tt
+	end
+
+	tt[key] = value
+
+	if net_playervar_debug:GetBool() then
+		Msg("[PNVar] ")
+		print("Set",id,key,value)
+	end
+end
+
+local function Get(id,key)
+	local tt = data_table[id]
+
+	return tt and tt[key]
+end
+
+net.Receive(Tag,function(len)
+	local id    = net.ReadUInt(16)
+	local key   = net.ReadString()
+	local _type = net.ReadUInt(8)
+	local value = net.ReadType(_type)
+
+	Set(id,key,value)
+	
+	hook.Call(Tag,nil,id,key,value)
+end)
+
+function PLAYER:SetNetData(key,value)
+	if self ~= LocalPlayer() then error("not implemented") end
+	net.Start(Tag)
+		net.WriteString(key)
+		net.WriteType(value)
+	net.SendToServer()
+end
+
+local lookup = {}
+function PLAYER:GetNetData(key)
+	local id = lookup[self]
+
+	if id == nil then
+		id = self:UserID()
+		lookup[self] = id
+	end
+
+	local tt = data_table[id]
+	
+	return tt and tt[key]
+end
